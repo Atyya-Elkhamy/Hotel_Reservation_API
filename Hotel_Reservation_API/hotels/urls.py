b034666db84fa9e038e6d37db17e28845ee34cc7
AtyyaElkhamy <atiaelkhamy55@gmail.com>
2025-04-15 22:03:32 +0200

    add static files

--- Hotel_Reservation_API/hotels/urls.py
+++ Hotel_Reservation_API/hotels/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('create/', HotelListView.as_view(), name='hotel-list'),
+    path('update/<int:pk>/', HotelUpdateView.as_view(), name='hotel-update'),
+    # path('rooms/', RoomListView.as_view(), name='room-list'),
+    # path('rooms/<int:pk>/', RoomUpdateView.as_view(), name='room-update'),
 ]
